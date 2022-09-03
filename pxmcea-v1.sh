140d662828907e2a1a9a4089da3da2dc99cdb035
Guy Francoeur <108816504+gf-eaton@users.noreply.github.com>
2022-09-03 02:26:52 -0400

    final version

--- pxmcea-v1.sh
+++ pxmcea-v1.sh
@@ -4,6 +4,8 @@
 # execution : wget -O - https://raw.githubusercontent.com/gf-eaton/procedures/main/pxmcea-v1.sh | sudo bash
 #
 # Step 1 certificate
+cd
+pwd
 sudo /bin/mkdir /usr/share/ca-certificates/Eaton
 sudo /bin/cat > /usr/share/ca-certificates/Eaton/EatonRootCA2.crt <<EOF
 -----BEGIN CERTIFICATE-----
@@ -53,6 +55,7 @@ sudo apt install -y git cmake build-essential curl libcurl4-openssl-dev libssl-d
 sleep 5
 #
 # Step 3 dot NET 6
+cd
 rm -fr /opt/dotnet
 wget -nc https://download.visualstudio.microsoft.com/download/pr/901f7928-5479-4d32-a9e5-ba66162ca0e4/d00b935ec4dc79a27f5bde00712ed3d7/dotnet-sdk-6.0.400-linux-arm64.tar.gz
 echo "please wait this may take a minute or two ..."
