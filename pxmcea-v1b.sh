d721332790e25ab2737e0c60795742b9b0aafbfa
Guy Francoeur <108816504+gf-eaton@users.noreply.github.com>
2022-09-22 14:23:34 -0400

    initial release

---
+++ pxmcea-v1b.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#
+# 2022-08-02 GF
+# must be ran as : root
+# execution : wget -O - https://raw.githubusercontent.com/gf-eaton/procedures/main/pxmcea-v1b.sh | bash
+#
+#-------------------------------------------------------------------------------------------------------
+# This script add the container capability.
+#-------------------------------------------------------------------------------------------------------
+#
+# Step 1 systemd-nspawn and a sample pxmcea container
+# systemd-nspawn
+apt install -y debootstrap systemd-container
+
+### Create a container named
+rm -fr /var/lib/machines/pxmcea
+mkdir -p /var/lib/machines/pxmcea
+debootstrap --include=systemd-container,systemd,dbus stable /var/lib/machines/pxmcea http://deb.debian.org/debian/
+
+chroot /var/lib/machines/pxmcea
+cat <<EOF >/etc/securetty
+pts/0
+pts/1
+pts/2
+pts/3
+EOF
+
+echo 'deb https://deb.debian.org/debian-security/ stable-security main' >> /etc/apt/sources.list
+echo 'pxmcea' > /etc/hostname
+
+echo 'export http_proxy="http://proxy.etn.com:8080"' > /etc/environment
+echo 'export https_proxy="http://proxy.etn.com:8080"' >> /etc/environment
+echo 'export no_proxy=" localhost, 127.0.0.1"' >> /etc/environment
+
+echo 'Defaults       env_keep+="http_proxy https_proxy no_proxy"' >> /etc/sudoers
+
+mkdir -p /usr/share/ca-certificates/Eaton
+cd /usr/share/ca-certificates/Eaton
+cat > EatonRootCA2.crt <<EOF
+-----BEGIN CERTIFICATE-----
+MIIFSjCCAzKgAwIBAgIQPpyid3XgppdPmbQ2Wkh4iDANBgkqhkiG9w0BAQsFADA2
+MRowGAYDVQQKExFFYXRvbiBDb3Jwb3JhdGlvbjEYMBYGA1UEAxMPRWF0b24gUm9v
+dCBDQSAyMB4XDTE1MDYwNTE1MTcwNVoXDTQwMDYwNTE1MTcwNVowNjEaMBgGA1UE
+ChMRRWF0b24gQ29ycG9yYXRpb24xGDAWBgNVBAMTD0VhdG9uIFJvb3QgQ0EgMjCC
+AiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAKTGkkzSsnRjsssnuV9Ve+Kr
+QM3ekf2Xg8cYvnIJ4838r/Lxn+OydrBU+2N+fSEoOldP46fnhdNohV5f2BJOUh/9
+3mh0ie1+qUFZsEGYMSzsyooSWyXOYowWWpvFiAuRhv0raWZ6cHOgsqmPsk276d8g
+V4L0ha0I0sMryGehnc3XH/CH1MkG49kSlw+ma+4/2utyk3YxtkEK4AeCYWXps3af
+vhicWlE3vex2KvYvaWYCcBiFI9qKcmkuLjHxEerskxIKd14AP39YgVLaCm9nR4zi
+ITDkoZ+ma1EhSlVQfClN1cvPAyw//72kLkSB13JzwSv+GBVvrUnGIrkIGMCbhpwW
+hv4svSqQYWpwQf7zItF4sQ8sO0B3wi286Upxz0QT0DXpbyFu1bFPAWGL9gbIoSBO
+kxEtvR8l+mNIKFpJwLY1En2fkuZ0B0p7qEgvjLJWqehHqlP7NvRCFNXWbfyjKCoK
+sos2CeYqd0nripYFcpewRSg+sOyJrAsbUdjwkp/uPT/apLhn5s9V8FmApKZ1OOEC
+3zGvPoB9aKExsKrA81XYwF9ku05svg4VY4q20idFtcaV558KuwdHCTdRvzpwRMf4
+s8s8agI09ua7FBHhCYemwz5LDEepyMODwHCCV3NHizAHYx6zBO8Xw5wDYTfaQ6a9
+UyGwHUaamGxfXV87r197AgMBAAGjVDBSMA4GA1UdDwEB/wQEAwIBBjAPBgNVHRMB
+Af8EBTADAQH/MB0GA1UdDgQWBBTKF7CwumwcvYocJ+4P6Olgm6aTBDAQBgkrBgEE
+AYI3FQEEAwIBADANBgkqhkiG9w0BAQsFAAOCAgEAhThpf2hZKtfxlnqQX95BuFBb
+4scXchGkmAGhMv1OVAc4cZVHLCQ6GfihA4+wILlTGJTDiGzfIFbGGpiAlbS4btE4
+lDX1fnp2UOZcat8FdIoFAd/uUS7XeNPTlN/p18y6cJWY7tJXIR0rgxbTNcmkWV7H
+wt4WQ2FLDfj4Cfu7eHZNi5f8r8RsHiXIJvFmE4H+RHqa2LfbqDdmtUknshtMTmxs
+6F9UJZbmW2cNnQDZAkoNAykhNG3wZCbujwhmILgt9L3UIuylkx2PgJrUdvFDC9Ak
+l6Bo4TLFg62E4OCZAacrkAAsLQJ+eQx5DMlArDQUxa1Dps/QyoNMes6blwj2x2w8
+heT4r7YSejnv3vMUgQFChM2gyMI3PrKJDWAs1HDRBtk/LRuh0xB3VETGOX9cRmrq
+OpPV95DAP5Hs7IWaXjiq7OoQX9yIdL7Z7gjjffQ36doIFtQ0LT2zCa2yiLGbNQIt
+cMFOrnYJYiw+xk6NMKlsI5h4kFelNT3kQkj4WgmWBoJJ3RLPMzyw4Kj2+HiJXUk3
+bJ4tGDuPB9KbsRWJ+zqFNmI1dWbP1udD6D/w5FyVxfJ82cxOGOcEQvblwVdlqgth
+fX/EmfBj5VNmjMPp7Y5G649GcZt4dnFr59EjxseJCDiZ0Lp8KijWYLghAI/p6JSh
+RGcJghQfeOb8EGnja9k=
+-----END CERTIFICATE-----
+EOF
+
+echo "Eaton/EatonRootCA2.crt" >> /etc/ca-certificates.conf
+update-ca-certificates
+
+exit
+# ---------------- exiting chroot
+
+machinectl enable pxmcea
+machinectl start pxmcea
+machinectl status pxmcea
+machinectl stop pxmcea
+
+systemctl set-property systemd-nspawn@pxmcea.service MemoryMax=2G
+systemctl set-property systemd-nspawn@pxmcea.service CPUQuota=200%
+
+echo "systemd-nspawn -D /var/lib/machines/pxmcea -U --machine pxmcea"
+echo "     then ... you can change root password with passwd root"
+
+# Finish step 1
