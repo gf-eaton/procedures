83279cf4f83dddd9ec3510b107f58058bcfcec0d
Guy Francoeur <108816504+gf-eaton@users.noreply.github.com>
2023-05-01 14:29:15 -0400

    a small very useful..

--- pxmcea-v2.sh
+++ pxmcea-v2.sh
@@ -12,6 +12,7 @@ echo "net.ipv6.conf.all.disable_ipv6=1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6=1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.lo.disable_ipv6=1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.all.disable_ipv6=1" >> /etc/sysctl.d/disable-ipv6.conf
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 #---------------------------------------------------------------------------------------------------------
 # Step 1 certificate
 cd
