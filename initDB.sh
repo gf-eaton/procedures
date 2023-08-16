d3a54d3d07194d48ae416351771a5217767f35ff
Guy Francoeur <108816504+gf-eaton@users.noreply.github.com>
2023-08-16 11:56:00 -0400

    Create initDB.sh

---
+++ initDB.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir -p /mnt/sda/data
+chown postgres:postgres /mnt/sda/data
+
+su - postgres -c "psql -c \"ALTER USER postgres WITH PASSWORD 'SetNewPasswordNow!';\" "
+su - postgres -c "createuser pxmcea"
+su - postgres -c "psql -c \"ALTER USER pxmcea WITH PASSWORD 'SetNewPasswordNow!';\" "
+su - postgres -c "createdb telemetry"
+su - postgres -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE telemetry TO pxmcea;\" "
+su - postgres -c "mkdir -p /mnt/sda/data/ts_archive_2023"
+su - postgres -c "psql -c \"CREATE TABLESPACE ts_archive_2023 OWNER pxmcea LOCATION '/mnt/sda/data/ts_archive_2023';\" "
+su - postgres -c "mkdir -p /var/lib/postgresql/13/data/ts_telemetry"
+su - postgres -c "psql -c \"CREATE TABLESPACE ts_telemetry OWNER pxmcea LOCATION '/var/lib/postgresql/13/data/ts_telemetry';\" "
+su - postgres -c "psql -d telemetry -U pxmcea -h localhost -c \"CREATE TABLE IF NOT EXISTS public.telemetry (rowid SERIAL NOT NULL, iot boolean DEFAULT false, ts timestamp without time zone, att bigint, val numeric(16,6), dwrite timestamp without time zone DEFAULT timezone('utc'::text, now())) WITH (autovacuum_enabled = TRUE) TABLESPACE ts_telemetry;\" "
+
+echo "You are done."
